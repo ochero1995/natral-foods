1ccb1a573c893512590b9c5e7f235fd17eafe6cc
Andrew <ochero1995@user.noreply.github.com>
2016-09-25 17:06:58 +0100

    Initial Commit

---
+++ script/script.js
@@ -0,0 +1,35 @@
+/* ================================= 
+  Responsive navbar JavaScript
+==================================== */
+
+function responsiveFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+
+/* ================================= 
+  Modal Javascript
+==================================== */
+
+var modal = document.getElementById('myModal');
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
